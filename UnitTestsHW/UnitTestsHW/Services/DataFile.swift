9805d5e2646d5e3f390dd8a8fffc924f06e065ba
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-01-11 02:09:34 +0300

    prt1
    - test class added

--- UnitTestsHW/UnitTestsHW/Services/DataFile.swift
+++ UnitTestsHW/UnitTestsHW/Services/DataFile.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 struct DataFile {
-  
+    private(set) var values = (firstValue: 0, secondValue: 0)
+    
+    func addition(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multiplication(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func dividing(x: Double, y: Double) throws -> Double {
+        if y == 0 {
+            throw NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Division by zero is imposible"])
+        }
+        return x / y
+    }
 }
